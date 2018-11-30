0760d4d0547577ec3d23fa45d7aa0de37a0e11c3
Plínio Naves <pliniopjn@gmail.com>
2018-11-30 19:16:41 -0200

    Animando um único elemento com <transition>

--- src/App.vue
+++ src/App.vue
@@ -35,28 +35,28 @@ export default {
 
   /* entrada */
   .v-enter {
-
+    opacity: 0;
   }
 
   .v-enter-active {
-
+    transition: opacity 1s;
   }
 
   .v-enter-to {
-
+    opacity: 1;
   }
 
   /* saída */
   .v-leave {
-
+    opacity: 1;
   }
 
   .v-leave-active {
-
+    transition: opacity 1s;
   }
 
   .v-leave-to {
-    
+    opacity: 0;
   }
 
 </style>
