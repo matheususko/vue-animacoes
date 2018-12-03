298e61e827523275cfbe1309c908ca5acc5d80f5
Plínio Naves <pliniopjn@gmail.com>
2018-12-03 14:35:03 -0200

    Criando animação de "zoom" com CSS Transitions

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 
       <button class="btn btn-primary mb-3" @click="mostrar = !mostrar">Alternar</button>
 
-      <transition name="fade">
+      <transition name="zoom">
         <div class="alert alert-primary" v-if="mostrar">Animações no Vue</div>
       </transition>
 
@@ -32,6 +32,14 @@ export default {
 </script>
 
 <style scoped>
+
+  .zoom-enter, .zoom-leave-to {
+    transform: scale(0);
+  }
+
+  .zoom-enter-active, .zoom-leave-active {
+    transition: transform 0.5s;
+  }
 
   .fade-enter, .fade-leave-to {
     opacity: 0;
